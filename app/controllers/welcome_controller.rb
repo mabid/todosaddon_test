f2bbf746efc3aca2e1ad3fd6199f8b0018db161d
Abid Mahmood <mabid.mahmood@gmail.com>
2013-02-15 23:47:33 +0500

    support when no addon

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
 
+  before_filter :has_addon
+
   def index
     @todos = addon_api.index
   end
@@ -21,6 +23,18 @@ class WelcomeController < ApplicationController
   def update
     addon_api.update(params[:id], params[:text]) 
     redirect_to action: :index
+  end
+
+  def no_addon
+    render text: "Looks like you still havn't installed the addon into your app. please install it and then come back to this page"
+  end
+
+  protected
+
+  def has_addon
+    if ENV['TODOSADDON_ACCESS_TOKEN'].nil? or ENV['TODOSADDON_URL'].nil?
+      redirect_to no_addon_path
+    end
   end
 
 end
